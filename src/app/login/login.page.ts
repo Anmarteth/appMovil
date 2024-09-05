2eb3f7c9ecc06cd100185348e8179023a2dcc206
anmarteth <ethicalanmart@gmail.com>
2024-09-04 23:46:15 -0400

    Login con restricciones y redireccionando al home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  username:string=""
+  password:string=""
+  constructor(public alerta:AlertController, public toast:ToastController, private router:Router) { }
 
   ngOnInit() {
   }
+  async mensajeCorrecto() {
+    const toast = await this.toast.create({
+      message: 'Inicio de sesion correcto',
+      duration: 2000
+    });
+    toast.present();
+  }
+  async mensajeError() {
+    const alert = await this.alerta.create({
+      header: 'Error de inicio',
+      message: 'No puede ingresar con campos vacios',
+      buttons: ['Aceptar']
+    });
+  
+    await alert.present();
+  }
+  ingresar(){
+    if(this.username===""||this.password===""){
+      console.log("no puede haber valores vacios")
+      this.mensajeError()
+    }
 
+    else{
+      console.log("inicio exitoso")
+      this.mensajeCorrecto()
+      this.router.navigate(["/home"])
+    }
+  }
 }
